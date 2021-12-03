f92ea3c24877c391d8c10a2127cbe637e333f732
Karen Sommer <ksommerh94@gmail.com>
2021-12-03 18:50:26 -0500

    Lugares model
    popup in lugares

--- TravelBlog/js/lugares.js
+++ TravelBlog/js/lugares.js
@@ -1,16 +1,34 @@
 var slideIndex = 1;
-showDivs(slideIndex);
+//get modal opening buttons
+var modal_btn = document.querySelectorAll('.modal-btn-open');
 
-//move slicer to slicer index -1 or +1
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+modal_btn.forEach(function(btn){
+  btn.onclick = function(){
+    var modal = btn.getAttribute("data-modal");
+    document.getElementById(modal).style.display = 'block';
+  };
+});
+
+
+var close_modal = document.querySelectorAll(".modal-btn-close");
+close_modal.forEach(function(btn){
+  btn.onclick = function(){
+    var modal = btn.closest(".modal-container").style.display = 'none';
+  };
+});
+
 window.addEventListener("DOMContentLoaded", event => {
+
+  showDivs(slideIndex);
   //When passing parameter values, use an "anonymous function" that calls the specified function with the parameters:
   document.getElementById("btn-prev").addEventListener("click", function(){ plusDivs(-1); });
   document.getElementById("btn-next").addEventListener("click", function(){ plusDivs(1); });
-})
 
+
+})
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
